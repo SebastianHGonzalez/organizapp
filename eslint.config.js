7902f25526200154cc7cd556832d9bff23bb5d5a
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2025-06-23 15:01:36 +0000

    Added linting

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+
+module.exports = defineConfig([
+  expoConfig,
+  eslintPluginPrettierRecommended,
+  {
+    ignores: ["dist/*", "/.expo", "node_modules", "coverage"],
+    languageOptions: {
+      globals: {
+        expect: "readonly",
+        global: "readonly",
+        describe: "readonly",
+        it: "readonly",
+        beforeEach: "readonly",
+        afterEach: "readonly",
+        beforeAll: "readonly",
+        afterAll: "readonly",
+        jest: "readonly",
+      },
+    },
+  },
+]);
